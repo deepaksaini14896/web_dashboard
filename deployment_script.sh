003f2e2c8196a2cd24842eb2b047a151d3530acf
deepak <deepaksaini14896@gmail.com>
2021-03-02 23:52:55 +0530

    new

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,4 +2,4 @@
 git pull origin master
 pip3 install -r requirements.txt
 python3 ./dashboard/manage.py migrate
-sudo supervisorctl restart web_dashboard
+sudo -S supervisorctl restart web_dashboard
