374f474f121365a9285c73df36985e66a2b061b7
Ubuntu <ubuntu@ip-172-31-40-181.ap-south-1.compute.internal>
2021-03-02 18:12:21 +0000

    new update

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-ssh ubuntu@ip-172-31-40-181 <<EOF
-  cd web_dashboard
-  git pull
-  pip3 install -r requirements.txt
-  ./dashboard/manage.py migrate
-  sudo supervisorctl restart web_dashboard
-  exit
-EOF
+git pull
+pip3 install -r requirements.txt
+python3 ./dashboard/manage.py migrate
+sudo supervisorctl restart web_dashboard
