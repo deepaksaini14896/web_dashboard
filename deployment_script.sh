20cc30493e45d3c20520a88f0e930999c4170015
deepak <deepaksaini14896@gmail.com>
2021-03-04 00:27:52 +0530

    update

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-
-exit
-cd web_dashboard
-
 git pull origin master
-
-
 pip3 install -r requirements.txt
 python3 ./dashboard/manage.py migrate
 sudo supervisorctl restart web_dashboard
