22b82c77eb2a96aa8b1d421d86abc280d3e09ffa
deepak <deepaksaini14896@gmail.com>
2021-03-03 12:37:05 +0530

    deployment file update

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,7 +2,4 @@
 git pull origin master
 pip3 install -r requirements.txt
 python3 ./dashboard/manage.py migrate
-sudo su    
-visudo -f /etc/sudoers
-jenkins ALL= NOPASSWD: ALL
-sudo -S supervisorctl restart web_dashboard
+sudo supervisorctl restart web_dashboard
